aaa5cba51d5dc545878a3bf10a047c9fa7b41515
John Munn <jmunn@getchalk.com>
2019-11-19 10:15:02 -0400

    Inital commit

---
+++ locations.js
@@ -0,0 +1,22 @@
+export default data = [
+    {
+        name:'Christophe',
+        likes: ["Lemon Tree", "Upstreet", "True North", "Ela"],
+        dislikes: ["Moxies", "Edible Matters"]
+    },
+    {
+        name:'Evan',
+        likes: [],
+        dislikes: ["Lemon Tree", "Edible Matters", "Ela"]
+    },
+    {
+        name:'Justin',
+        likes: ["Lemon Tree", "Upstreet", "True North", "Cha Ba Thai", "Finbars"],
+        dislikes: ["Jack Astors", "East Side Marios", "Moxies", "Boston Pizza"]
+    },
+    {
+        name:'Kelly',
+        likes: ["Cheachies", "Dhaba", "Lemon Tree", "Moxies", "Upstreet"],
+        dislikes: ["Subway"]
+    }
+]
